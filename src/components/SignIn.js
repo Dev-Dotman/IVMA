a0e023450a493d8292900ad90897fbcb0ca5124d
Dev-Dotman <olaiyadotun05@gmail.com>
2025-11-21 21:14:56 +0100

    removed social login

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -230,7 +230,7 @@ export default function SignIn({ onToggleMode }) {
             </div>
 
             {/* Social Login */}
-            <div className="flex justify-center pt-4">
+            {/* <div className="flex justify-center pt-4">
               <button className="flex items-center justify-center gap-3 w-full px-4 py-3 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors">
                 <svg className="w-5 h-5" viewBox="0 0 24 24">
                   <path
@@ -254,7 +254,7 @@ export default function SignIn({ onToggleMode }) {
                   Sign in with Google
                 </span>
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -286,7 +286,7 @@ export default function SignUp({ onToggleMode }) {
             </form>
 
             {/* Social Login */}
-            <div className="flex justify-center pt-4">
+            {/* <div className="flex justify-center pt-4">
               <button className="flex items-center justify-center gap-3 w-full px-4 py-3 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors">
                 <svg className="w-5 h-5" viewBox="0 0 24 24">
                   <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
@@ -296,7 +296,7 @@ export default function SignUp({ onToggleMode }) {
                 </svg>
                 <span className="text-gray-700 font-medium">Sign up with Google</span>
               </button>
-            </div>
+            </div> */}
 
             <div className="text-center">
               <p className="text-sm text-gray-600">
