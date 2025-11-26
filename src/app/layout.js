7a2d63cdee31278944759cb8ef35e403d2fae4db
Dev-Dotman <olaiyadotun05@gmail.com>
2025-11-27 00:29:15 +0100

    Favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,17 +2,56 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Providers } from "./providers";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IVMA - Inventory Management System",
-  description: "Professional inventory management system for businesses",
+  description: "Professional business management system for businesses",
+  icons: {
+    icon: [
+      {
+        url: "/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        type: "image/png",
+        sizes: "180x180",
+      },
+    ],
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+      </Head>
       <body className={inter.className}>
         <AuthProvider>
           <Providers>{children}</Providers>
